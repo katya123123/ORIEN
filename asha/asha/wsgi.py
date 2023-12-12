3bd3a19ac7d43277154e0f37728e4e0b10845808
katya123123 <katyku221133@gmail.com>
2023-12-13 00:27:14 +1000

    second

--- asha/asha/wsgi.py
+++ asha/asha/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'asha.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{{asha}}.settings')
 
 application = get_wsgi_application()
